f95b08492931f95b862a3f225e6de4949f46db7d
Instagram-user-identity <mrpiyushchaudhari2006@gmail.com>
2026-04-08 17:50:00 +0530

    practise

---
+++ Day 1/dsa.js
@@ -0,0 +1,126 @@
+// Q. Print natural number up to n.
+
+// let num = 5
+// for(let i=1;i<=num;i++){
+//     console.log(i + " ");
+// }
+
+
+// Q. Reverse for loop. Print n to 1.
+
+// let n = 5
+// for(let i = n ; i>0;i--){
+//     console.log(i);
+    
+// }
+
+
+
+// Q.Take a number as input and print its table
+//      5 * 1 = 5
+//      5 * 2 = 10 ... up to 10 terms
+
+
+// let prompt = require('prompt-sync')()
+// let num = Number(prompt("Enter a table:-"))
+// for(let i=1;i<=10;i++){
+//     console.log(num + " * " +i + " = " + num*i );
+    
+// }
+
+
+// Q.Sum up to n terms
+
+// let n=6
+// let sum = 0
+// for(let i=1;i<=n;i++){
+//     sum = sum + i
+// }
+// console.log(sum);
+
+
+
+// Q. Factorial of a number
+
+// let n = 5
+// let fact = 1
+// for(let i=1;i<=n;i++){
+//     fact = fact * i
+
+// }
+
+// console.log(fact);
+
+// Q. Print the sum of all even & odd numbers in a range seperately.
+
+// let even = 0
+// let odd = 0
+// let num = 10
+// for(let i=1;i<=num;i++){
+//     if(i % 2 == 0){
+//         even = even+i
+//     }
+//     else{
+//         odd = odd + i
+//     }
+// }
+
+// console.log("even===",even);
+// console.log("odd===",odd);
+
+
+
+
+// Q.Print all the factors of a number.
+
+// let n = 20
+// for(let i=0;i<n;i++){
+//     if(n%i == 0){
+//         console.log(i);
+        
+//     }
+// }
+
+
+// Q.Check if the number is Prime or not.
+
+// let num = 11
+// let isPrime = true
+
+// if(num <= 0) console.log("not valid");
+// if(num == 1) console.log("prime");
+
+
+
+// for(let i=2;i<num;i++){
+//     if(num % i == 0){
+//         isPrime = false
+//         break
+//     }
+//     isPrime = true
+// }
+
+// if(isPrime) console.log("prime");
+// else console.log("not Prime");
+
+
+// Q. Write a program to take two inputs a, b & find the value of a  raised to the power of b.
+//      Ex - a = 2, b = 5 
+//      OP - 2^5 = 32
+
+
+// let a = 2
+// let b = 5
+
+// let result = a**b
+// console.log(result);
+
+// let a = 2
+// let b = 5
+// let val = 1
+// for(let i = 0 ; i<b;i++){
+//     val = val*a
+// }
+
+// console.log(val);
+
---
+++ Day 1/readme.md
@@ -0,0 +1,37 @@
+# 📘 JavaScript Basics – Loops & Number Programs
+
+---
+
+
+
+## 📂 Solve problem
+
+### 🔹 1. Print Natural Numbers
+Print numbers from `1` to `n`.
+
+### 🔹 2. Reverse Loop
+Print numbers from `n` to `1`.
+
+### 🔹 3. Multiplication Table
+Generate table of a number up to 10.
+
+### 🔹 4. Sum of First N Numbers
+Calculate sum from `1` to `n`.
+
+### 🔹 5. Factorial of a Number
+Find factorial using loop.
+
+### 🔹 6. Sum of Even & Odd Numbers
+Calculate even and odd sums separately.
+
+### 🔹 7. Factors of a Number
+Print all factors of a number.
+
+### 🔹 8. Prime Number Check
+Check whether a number is prime or not.
+
+### 🔹 9. Power Calculation
+Find `a^b` using:
+- Exponent operator
+- Loop method
+
