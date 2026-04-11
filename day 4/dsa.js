f4828f7c2d464963db54ae3436b850536364565f
Instagram-user-identity <mrpiyushchaudhari2006@gmail.com>
2026-04-11 17:52:17 +0530

    practise

---
+++ day 4/dsa.js
@@ -0,0 +1,61 @@
+// Accept value from user and assign in the array
+
+// const prompt = require('prompt-sync')()
+// const num = Number(prompt("Enter a Number"))
+
+// const arr = new Array(num)
+
+// for(let i=0;i<arr.length;i++){
+//     arr[i] = Number(prompt("Enter a Number:- "))
+// }
+
+// console.log(arr);
+
+
+//  Q. Sum of array’s element
+
+// let prompt = require('prompt-sync')()
+// let num = Number(prompt("Enter A number:-"))
+
+// const arr = new Array(num)
+// let sum = 0
+// for(let i=0;i<arr.length;i++){
+//     arr[i] = Number(prompt(`Enter a value of arr[${i}] :- `))
+//     sum += arr[i]
+// }
+// console.log(sum);
+
+
+
+// Max element from array
+
+// let arr = [2,4,5,3,9,8]
+// let max = 0
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>max){
+//         max =arr[i]
+//     }
+// }
+// console.log(max);
+
+
+// Second max element from array
+
+// let arr = [1,2,5,8,3,9]
+
+// let max = Math.max(arr[0],arr[1])
+// let smax = Math.min(arr[0],arr[1])
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>max){
+//         smax = max
+//         max = arr[i]
+//     }
+//     else if(arr[i]>smax && arr[i] != max){
+//         smax = arr[i]
+
+//     }
+    
+// }
+// console.log(smax);
---
+++ day 4/readme.md
@@ -0,0 +1,25 @@
+
+## 📌 Programs Included
+
+### 🔹 1. Accept Array from User
+- Takes input from user
+- Stores values in an array
+
+---
+
+### 🔹 2. Sum of Array Elements
+- Adds all elements of array
+- Displays total sum
+
+---
+
+### 🔹 3. Maximum Element in Array
+- Finds the largest number in array
+
+---
+
+### 🔹 4. Second Maximum Element
+- Finds second largest number
+- Uses efficient comparison logic
+
+---
